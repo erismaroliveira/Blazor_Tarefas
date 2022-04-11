d3298803a3fc2ed1c00399475c63ea0f2ae78142
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-04-11 10:43:58 -0300

    Invocando código C# via Javascript

--- wwwroot/JSFunctions.js
+++ wwwroot/JSFunctions.js
@@ -1,3 +1,10 @@
 window.MostraAlerta = (message) => {
     alert(message);
 }
+
+function getTotalTarefas() {
+    DotNet.invokeMethodAsync("Blazor_Tarefas", "ObterTotalTarefas")
+        .then(resultado => {
+            alert("Total de Tarefas: " + resultado);
+        });
+}
